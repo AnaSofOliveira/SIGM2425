bd11e13411752f54f5dacaf077740011cff5bc71
Eduardo Santos <43915860+ersantos1991@users.noreply.github.com>
2024-10-17 21:14:45 +0100

    ex1

--- aulasPraticas/aulaPratica4/Aula04_TRAJECTORIA/scripts/_go.bat
+++ aulasPraticas/aulaPratica4/Aula04_TRAJECTORIA/scripts/_go.bat
@@ -1,13 +1,15 @@
 @ECHO OFF
 :: [PTS: AJUSTAR]
-set psqlPath="C:\myApp\PostgreSQL\10\bin"
+set psqlPath="C:\Program Files\PostgreSQL\16\bin"
 
 :: Base de Dados e nome do utilizador
 SET dataBase=postgres
 SET userName=postgres
+SET portNumber=5432
 
 :: psql -h host -p port -d database -U user -f psqlFile
-%psqlPath%\psql -h localhost -p 5432 -d %dataBase% -U %userName% -f %1
+%psqlPath%\psql -h localhost -p %portNumber% -d %dataBase% -U %userName% -f %1
+
 
 
 
